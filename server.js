de2d5d196dacb813dd66a5dbea8c2df5c4353bda
joshuamitchell <joshuamitchell176@gmail.com>
2019-11-18 14:06:41 -0500

    updated

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ app.use(express.json());
 app.use(express.static('public'));
 
 const bands = [
-    {id:1, name:"Green Day", date:"1986", genre:"Pop Punk", active:"Yes"},
-    {id:2, name:"Iron Maiden", date:"1975", genre:"Heavy Metal", active:"Yes"},
-    {id:3, name:"Nirvana", date:"1987", genre:"Grunge", active:"No"},
-    {id:4, name:"Metallica", date:"1981", genre:"Thrash Metal", active:"Yes"},
-    {id:5, name:"Modest Mouse", date:"1992", genre:"Indie", active:"Yes"},
-    {is:6, name:"Kero Kero Bonito", date:"2011", genre:"Indie Pop", active:"Yes"}
+    {id:1, name:"Green Day", date:"1986", genre:"Pop Punk", active:"Active"},
+    {id:2, name:"Iron Maiden", date:"1975", genre:"Heavy Metal", active:"Active"},
+    {id:3, name:"Nirvana", date:"1987", genre:"Grunge", active:"Not Active"},
+    {id:4, name:"Metallica", date:"1981", genre:"Thrash Metal", active:"Active"},
+    {id:5, name:"Modest Mouse", date:"1992", genre:"Indie", active:"Active"},
+    {is:6, name:"Kero Kero Bonito", date:"2011", genre:"Indie Pop", active:"Active"}
 ]
 
 app.get('/api/bands', (req,res)=>{
